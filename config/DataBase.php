4dbda8a31651a37368d0394623433ec362bdf913
JuanDepv <74514584+JuanDepv@users.noreply.github.com>
2021-06-29 08:51:29 -0500

    comment a singleton patternt implements...

--- config/DataBase.php
+++ config/DataBase.php
@@ -27,3 +27,46 @@ class DataBase {
     }
 
 } 
+
+/*
+class DataBase {
+    private $host = "localhost";
+    private $db = "demoproject";
+    private $user = "root";
+    private $pass = "";
+    private $charset = NULL;
+    private $opt = NULL;
+    private $dsn = NULL;
+    private $connection = NULL;
+    private static $database = NULL;
+    
+    /* Private construct that can only be accessed from within this class
+    private function __construct() {
+        $this->createConnection();
+    }
+    
+    private function createConnection():void {
+        $this->charset = "utf8mb4";
+        $this->dsn = "mysql:host=$this->host;dbname=$this->db;charset=$this->charset";
+        $this->opt = [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES => false,
+        ];
+        $this->connection = new PDO($this->dsn, $this->user, $this->pass, $this->opt);
+    }
+    
+    /* A static method that will create an object instance once and after that it will reuse the same instance for all other requests
+    public static function getInstance():Database {
+        if (NULL == self::$database) {
+            self::$database = new Database();
+        }
+        return self::$database;
+    }
+    
+    /* A little getter function to access the connection object 
+    public function getConnection():PDO {
+        return $this->connection;
+    }
+}
+*/
